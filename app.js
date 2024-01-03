695af3d5c60fa1134feb757199dc8bb89dba6177
Aleksey Yuriev <alex11-90@yandex.by>
2024-01-03 20:28:57 +0300

    feat: mongoose connected

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
+
+mongoose.connect(MONGO_URL)
+  .then(() => console.log('MongoDB in process'))
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
