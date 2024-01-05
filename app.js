d605c2fcc414ea807be0cd288b83d961b983c558
Aleksey Yuriev <alex11-90@yandex.by>
2024-01-05 21:54:06 +0300

    hotfix: bugs fixed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const { errors } = require('celebrate');
+const json = require('express').json();
 const helmet = require('helmet');
 const handleError = require('./middlewares/handleError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -12,13 +13,13 @@ dotenv.config();
 
 const app = express();
 
-const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { NODE_ENV, PORT, MONGO_URL } = process.env;
 
-mongoose.connect(MONGO_URL)
+mongoose.connect(NODE_ENV ? MONGO_URL : 'mongodb://127.0.0.1:27017/bitfilmsdb')
   .then(() => console.log('MongoDB in process'))
   .catch((err) => console.error('Error connecting to MongoDB:', err));
 
-app.use(express.json());
+app.use(json);
 app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);
 app.use(helmet());
@@ -26,8 +27,8 @@ app.use(limiter);
 app.use(router);
 app.use(errorLogger);
 app.use(errors());
-// app.use(handleError);
+app.use(handleError);
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+  console.log(`App listening on port ${NODE_ENV ? PORT : 3000}`);
 });
